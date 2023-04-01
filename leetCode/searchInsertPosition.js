f0702a5197447e755608a35f1b78895c43beeb8d
Maryam-bit <rajputmaryam5555@gmail.com>
2023-04-01 12:53:00 +0500

    search Insert position

--- leetCode/searchInsertPosition.js
+++ leetCode/searchInsertPosition.js
@@ -1,7 +1,7 @@
 var searchInsert = function(nums, target) {
     let left = 0;
     let right = nums.length-1
-    while(left<right) {
+    while(left<=right) {
         const mid = Math.floor((left+right)/2);
         if(nums[mid] == target) {
             return mid;
@@ -16,8 +16,8 @@ var searchInsert = function(nums, target) {
     if(target > nums[right]) {
         return right + 1
     }
-    else if (target > nums[left]) {
-        return left - 1
+    else if (target < nums[left]) {
+        return left
     }
     else {
         return left + 1
