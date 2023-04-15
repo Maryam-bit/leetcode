c13e396ad492a9f9f449c9fe79181da90d042b55
Maryam-bit <rajputmaryam5555@gmail.com>
2023-04-15 23:52:26 +0500

    Add Two Integers - LEETCODE

--- easy/addTwoIntegers.js
+++ easy/addTwoIntegers.js
@@ -1,3 +1,17 @@
+/*
+========== Question:  =========
+Add Two Integers
+
+========== Description: =========== 
+Given two integers num1 and num2, return the sum of the two integers.
+
+========== Example 1: ============ 
+Input: num1 = 12, num2 = 5
+Output: 17
+== Explanation: == 
+num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+*/
+
 var sum = function(num1, num2) {
     return num1 + num2;
 };
