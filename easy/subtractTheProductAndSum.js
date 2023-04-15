ee69b2c81adf2e069a9e6ad65d11268dfbd5cbba
Maryam-bit <rajputmaryam5555@gmail.com>
2023-04-15 23:30:00 +0500

    Subtract The Product And Sum - LEETCODE

--- easy/subtractTheProductAndSum.js
+++ easy/subtractTheProductAndSum.js
@@ -1,15 +1,19 @@
-// ======= Subtract the Product and Sum of Digits of an Integer 💡 ========
+/*
+========== Question:  =========
+Subtract the Product and Sum of Digits of an Integer
 
-/* 
+========== Description: =========== 
 Given an integer number n return the difference between the product of its digits and the sum of its digits
-Example 1: 
+
+========== Example 1: ============ 
 Input: n = 234
 Output: 15 
-Explanation: 
+== Explanation: == 
 Product of digits = 2 * 3 * 4 = 24 
 Sum of digits = 2 + 3 + 4 = 9 
 Result = 24 - 9 = 15
 */
+
 
 var subtractProductAndSum = function (n) {
     const product = eval(n.toString().split('').join('*'))
