2aef0b8cce837c55cce0da450d095c7161697ceb
Maryam-bit <rajputmaryam5555@gmail.com>
2023-04-16 00:21:25 +0500

    Search Insert Position - LEETCODE

--- easy/13.SearchInsertPosition.js
+++ easy/13.SearchInsertPosition.js
@@ -1,3 +1,17 @@
+/*
+========== Question:  =========
+Search Insert Position
+
+========== Description: =========== 
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+========== Example 1: ============ 
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+*/
+
+
 var searchInsert = function(nums, target) {
     let left = 0;
     let right = nums.length-1
