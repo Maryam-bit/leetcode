707e032fa63a7211e5055106a0f53ef41f99e7cb
Maryam-bit <rajputmaryam5555@gmail.com>
2023-08-31 00:22:22 +0500

    29. Remove Element

---
+++ easy/29.RemoveElement.js
@@ -0,0 +1,8 @@
+var removeElement = function(nums, val) {
+    for(i=0; i<=nums.length; i++) {
+         if(nums[i] == val) {
+             nums.splice(i, 1)
+             i--;
+         }
+     } 
+ };
