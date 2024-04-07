1cbdca65beb116b7c908bb91b51db36a04a325df
Maryam-bit <rajputmaryam5555@gmail.com>
2024-04-08 00:02:20 +0500

    9.ReverseWordsInString

---
+++ medium/9.ReverseWordsInString.js
@@ -0,0 +1,30 @@
+/*
+========== Question:  =========
+151. Reverse Words in a String
+
+========== Description: =========== 
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+========== Example 1: ============  
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+
+========== Example 2: ============  
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+*/
+
+
+var reverseWords = function(s) {
+    return s.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ');
+};
+
+const s = "a good   example";
+console.log(reverseWords(s))
