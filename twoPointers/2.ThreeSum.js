b0b1524182b4206f7c9478fe3f2c7ee5417ccc4c
Maryam-bit <rajputmaryam5555@gmail.com>
2024-04-13 16:49:30 +0500

    2.ThreeSum

--- twoPointers/2.ThreeSum.js
+++ twoPointers/2.ThreeSum.js
@@ -1,20 +1,26 @@
 /*
 ========== Question:  =========
-167. Two Sum II - Input Array Is Sorted
+15. 3Sum
 
 ========== Description: =========== 
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
-
-Your solution must use only constant extra space.
+Notice that the solution set must not contain duplicate triplets.
 
 ========== Example 1: ============ 
-Input: numbers = [2,7,11,15], target = 9
-Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+========== Example 2: ============ 
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
 */
 
 var threeSum = function(numbers) {
@@ -30,14 +36,14 @@ var threeSum = function(numbers) {
         let r = numbers.length-1
         
         while(l<r) {
-                const sum = numbers[i] + numbers[l] + numbers[r];
-                if (sum < 0) l++;
-                else if (sum > 0) r--
-                else {
-                    result.push([numbers[i], numbers[l], numbers[r]])
-                    l++;
-                    while(numbers[l] === numbers[l-1] && l<r) l++;
-                }
+            const sum = numbers[i] + numbers[l] + numbers[r];
+            if (sum < 0) l++;
+            else if (sum > 0) r--
+            else {
+                result.push([numbers[i], numbers[l], numbers[r]])
+                l++;
+                while(numbers[l] === numbers[l-1] && l<r) l++;
+            }
         }
     }
     return result;
