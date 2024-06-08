42a631129a8fad86d47981f5148fad3cab7abf54
Maryam-bit <rajputmaryam5555@gmail.com>
2024-06-08 17:41:03 +0500

    1. Ransom Note

--- hashmap/1.RansomNote.js
+++ hashmap/1.RansomNote.js
@@ -1,8 +1,16 @@
-/**
- * @param {string} ransomNote
- * @param {string} magazine
- * @return {boolean}
- */
+/*
+========== Question:  =========
+383. Ransom Note
+
+========== Description: =========== 
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+========== Example 1: ============  
+Input: ransomNote = "a", magazine = "b"
+Output: false
+*/
 
 // ========== solution 1 
 var canConstruct = function(ransomNote, magazine) {
